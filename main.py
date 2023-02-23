9482586f84991942b26af7073c3f4ceb31a75c00
XXX-Balystic-Badila-999-XXX <119594594+XXX-Balystic-Badila-999-XXX@users.noreply.github.com>
2023-02-23 12:50:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import saluti_tiepo
+import saluti_carli
+
+def saluuti_paesanti_gruppo4():
+   print("Saluti da Paesanti Andrea")
+
+def main():
+   saluti_tiepo.saluto_tiepo_Gruppo4();
+   saluti_carli.saluto_carli_Gruppo4();
+    
+    
+main()
